6304cba33ff7855d0b6886647d20eca81d79296c
chaithanya <chaithanyack656@gmail.com>
2025-07-02 21:39:47 +0530

    roboshop code

--- user.sh
+++ user.sh
@@ -5,11 +5,12 @@ dnf install nodejs -y
 cp user.service /etc/systemd/system/user.service
 
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
 unzip /tmp/user.zip
-cd /app
+
 npm install
 
 
