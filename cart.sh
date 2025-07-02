a199f3fa5b621057851b89d164dda519b384f825
chaithanya <chaithanyack656@gmail.com>
2025-07-02 17:45:58 +0530

    roboshop code

--- cart.sh
+++ cart.sh
@@ -6,7 +6,7 @@ cp cart.service /etc/systemd/system/cart.service
 
 useradd roboshop
 mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 cd /app
