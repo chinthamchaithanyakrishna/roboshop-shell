cbf6c7cbdd7fdd0031fc7ae018a1a7ef8ea57e31
chaithanya <chaithanyack656@gmail.com>
2025-07-04 17:38:46 +0530

    roboshop code

--- common.sh
+++ common.sh
@@ -1,21 +1,37 @@
+APP_PREREQ(){
+    cp ${component}.service /etc/systemd/system/${component}.service
+    useradd roboshop
+    rm -rf /app
+    mkdir /app
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+    cd /app
+    unzip /tmp/${component}.zip
+}
+
+SYSTEMD(){
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl start ${component}
+}
+
 NODEJS() {
   dnf module disable nodejs -y
   dnf module enable nodejs:20 -y
   dnf install nodejs -y
 
-  cp ${component}.service /etc/systemd/system/${component}.service
-
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
-  cd /app
-  unzip /tmp/${component}.zip
+  APP_PREREQ
   npm install
 
+  SYSTEMD
 
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl start ${component}
 
+}
+
+PYTHON(){
+  dnf install python3 gcc python3-devel -y
+
+  APP_PREREQ
+  pip3 install -r requirements.txt
+
+  SYSTEMD
 }
