ab46cc02513418314dfd72e3bbf53f8ec86b9d3f
chaithanya <chaithanyack656@gmail.com>
2025-07-04 17:12:12 +0530

    roboshop code

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,6 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-
-cp catalogue.service /etc/systemd/system/catalogue.service
-
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-
-npm install
+source common.sh
+component=catalogue
+NODEJS
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.chaithanya.online </app/db/master-data.js
