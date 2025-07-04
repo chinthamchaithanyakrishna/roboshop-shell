3ad1d9ea21ec5ba58b6efaff27b23769054a54d9
chaithanya <chaithanyack656@gmail.com>
2025-07-04 18:00:27 +0530

    roboshop code

--- common.sh
+++ common.sh
@@ -43,10 +43,6 @@ MAVEN(){
   mvn clean package
   mv target/shipping-1.0.jar shipping.jar
 
-  dnf install mysql -y
-  mysql -h mysql-dev.chaithanya.online -uroot -pRoboShop@1 < /app/db/schema.sql
-  mysql -h mysql-dev.chaithanya.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-  mysql -h mysql-dev.chaithanya.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
   SYSTEMD
 }
